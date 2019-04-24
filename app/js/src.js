47abda3e89084a363e8b65cdfbd538ce6f9f1f98
YuriyChob <sonicmaster172@gmail.com>
2019-04-24 17:33:44 +0300

    header and some fixes

--- app/js/src.js
+++ app/js/src.js
@@ -5,7 +5,7 @@ $(".js-range-slider").ionRangeSlider({
   step: 10,
   hide_from_to: true,
   hide_min_max: true,
-  
+
   onChange: function (data) {
     console.dir(data.from);
 
@@ -14,3 +14,33 @@ $(".js-range-slider").ionRangeSlider({
   }
 
 });
+
+//checking if target is not one of nodelist elements`
+function check(nodelist, target){
+  var result = [].every.call(nodelist, function(item){
+    return item != target;
+  });
+  return result;
+};
+
+var searchButton = document.querySelector('.header-search-button');
+var submitButton = document.querySelector('.header-submit-button');
+var headerSearch = document.querySelector('.header-search-input');
+searchButton.addEventListener('click', function(){
+  headerSearch.classList.add('search-is-active');
+  submitButton.classList.add('submit-btn-is-active');
+});
+
+document.querySelector('body').onclick = function(e){
+  if(e.target != $('.header-search form') && check($('.header-search form *'), e.target)){
+    headerSearch.classList.remove('search-is-active');
+    submitButton.classList.remove('submit-btn-is-active');
+  }
+};
+
+var menuOpener = document.querySelector('.menu-opener');
+console.log(menuOpener.classList);
+var topNav =  document.querySelector('.top-nav');
+menuOpener.addEventListener('click', function(){
+  topNav.classList.toggle('menu-is-active');
+})
