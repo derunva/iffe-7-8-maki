bc0241786049e7160ccfa6a7e755e7c70912132d
YuriyChob <sonicmaster172@gmail.com>
2019-04-24 17:52:21 +0300

    header and footer fixes

--- app/js/src.js
+++ app/js/src.js
@@ -26,6 +26,9 @@ function check(nodelist, target){
 var searchButton = document.querySelector('.header-search-button');
 var submitButton = document.querySelector('.header-submit-button');
 var headerSearch = document.querySelector('.header-search-input');
+var menuOpener = document.querySelector('.menu-opener');
+var topNav =  document.querySelector('.top-nav');
+
 searchButton.addEventListener('click', function(){
   headerSearch.classList.add('search-is-active');
   submitButton.classList.add('submit-btn-is-active');
@@ -35,12 +38,12 @@ document.querySelector('body').onclick = function(e){
   if(e.target != $('.header-search form') && check($('.header-search form *'), e.target)){
     headerSearch.classList.remove('search-is-active');
     submitButton.classList.remove('submit-btn-is-active');
-  }
+  };
+  if(check($('.top-nav *'), e.target) && check($('.menu-opener *'), e.target) && e.target != menuOpener){
+    topNav.classList.remove('menu-is-active');
+  };
 };
 
-var menuOpener = document.querySelector('.menu-opener');
-console.log(menuOpener.classList);
-var topNav =  document.querySelector('.top-nav');
 menuOpener.addEventListener('click', function(){
   topNav.classList.toggle('menu-is-active');
 })
