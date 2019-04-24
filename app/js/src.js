696494e3928a4086156923b5ac008168a0088eb1
Cassidy <icassidyif@gmail.com>
2019-04-24 16:23:31 +0300

    fix_search_and_sidebar

--- app/js/src.js
+++ app/js/src.js
@@ -5,7 +5,7 @@ $(".js-range-slider").ionRangeSlider({
   step: 10,
   hide_from_to: true,
   hide_min_max: true,
-  
+
   onChange: function (data) {
     console.dir(data.from);
 
@@ -14,3 +14,25 @@ $(".js-range-slider").ionRangeSlider({
   }
 
 });
+
+
+//  search-block
+var searchButton = $('.search > button');
+var search = $('.search');
+search.each(function(){
+  var currentSearch = $(this);
+  if(!(currentSearch.hasClass('is-active'))){
+    var btn = currentSearch.find('> button');
+    btn.on('click', function(){
+      currentSearch.addClass('is-active')
+    });
+    $(window).click(function(){
+      currentSearch.removeClass('is-active');
+    });
+    currentSearch.click(function(event){
+      event.stopPropagation();
+    });
+  };
+});
+
+
