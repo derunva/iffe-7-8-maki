d6bbe75e3c3413f20a0a9b5b608e0e3b7269c068
viktoria-shp <vvitashp@gmail.com>
2019-05-08 20:30:05 +0300

    response

--- app/js/src.js
+++ app/js/src.js
@@ -129,7 +129,7 @@ $(function () {
 });
 
 
-=======
+
 
 //slider
 
@@ -168,4 +168,28 @@ $('.slider-container').slick({
     // instead of a settings object
   ]
 });
+
+jQuery(document).ready(function(){
+    $('.qtyplus').click(function(e){
+        e.preventDefault();
+        fieldName = $(this).attr('field');
+        var currentVal = parseInt($('input[name='+fieldName+']').val());
+        if (!isNaN(currentVal)) {
+            $('input[name='+fieldName+']').val(currentVal + 1);
+        } else {
+            $('input[name='+fieldName+']').val(0);
+        }
+    });
+    $(".qtyminus").click(function(e) {
+        e.preventDefault();
+        fieldName = $(this).attr('field');
+        var currentVal = parseInt($('input[name='+fieldName+']').val());
+        if (!isNaN(currentVal) && currentVal > 0) {
+            $('input[name='+fieldName+']').val(currentVal - 1);
+        } else {
+            $('input[name='+fieldName+']').val(0);
+        }
+    });
+});
+
 
